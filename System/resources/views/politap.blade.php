d57cbec9f2d7addbae9e779e054546bf357435d6
Firmansyah <firman.institute0058@gmail.com>
2022-06-14 09:00:44 +0700

    project1

---
+++ System/resources/views/politap.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POLITEKNIK NEGERI KETAPANG</title>
+</head>
+<body>
+    <a href="">BERANDA</a>
+    <a href="">GALERI</a> |
+    <a href="">PROFIL</a> |
+    <a href="">BERITA</a> |
+    <h2>POLITAP</h2>
+    <p>
+        Politap dirintis pendiriannya pada tanggal 12 Februari 2006 berdasarkan Kesepakatan Bersama Nomor: 425.1/Ekbangsos-C dan Nomor: 03/YPI-KTP/B/02/2006 yang ditandatangani antara Bupati Kabupaten Ketapang (H. Morkes Effendi, S.Pd.) dengan Ketua Yayasan Pangeran Iranata Ketapang (Abdulbad H. A. Rani, S.Pd.). Legalitas operasional pendirian dan pembukaan program studi di Politap didasarkan pada Keputusan Menteri Pendidikan Nasional Republik Indonesia Nomor: 66/D/O/2008 tanggal 8 April 2008. Pada tahap awal pendirian, Politap didanai dengan dana sharing hibah dari Pemerintah Kabupaten Ketapang dan Direktorat Jenderal Pendidikan Tinggi Departemen Pendidikan Nasional (Ditjen Dikti Depdiknas). Program studi yang dibuka pada tahap awal terdiri dari tiga program studi dengan jenjang pendidikan Diploma Tiga (D3), yaitu: (1) Perawatan dan Perbaikan Mesin, (2) Teknik Pertambangan, dan (3) Teknologi Pengolahan Hasil Perkebunan. Program studi-program studi tersebut masing-masing bernaung di bawah jurusan Teknik Mesin, Teknik Pertambangan dan Teknologi Pertanian. Untuk memperkokoh pendirian Politeknik Ketapang ditetapkan Perda Kabupaten Ketapang No. 17 Tahun 2009
+    </p>
+</body>
+</html>
